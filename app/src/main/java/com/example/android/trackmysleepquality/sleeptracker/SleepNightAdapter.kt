d3f6eee90808e794e765aa0e3bc295c94a965634
JulioCesar415 <35044318+JulioCesar415@users.noreply.github.com>
2021-09-17 15:30:53 -0700

    SleepNightAdapter.kt

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -1,17 +1,22 @@
 package com.example.android.trackmysleepquality.sleeptracker
 
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.database.SleepNight
 
-class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
-//    define data source that holds list of SleepNight
-    var data = listOf<SleepNight>()
 
-    //    recycler view needs to know how big to make the scrollbar. this is how to tell recycler view how many views there are
+class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>(){
+
+    var data = listOf<SleepNight>
+
     override fun getItemCount() = data.size
 
     override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
         val item = data[position]
         holder.textView.text = item.sleepQuality.toString()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+        TODO("Not yet implemented")
     }
 }

98f58538f8226b007763a5dedef5207f17dfaff4
JulioCesar415 <35044318+JulioCesar415@users.noreply.github.com>
2021-09-17 20:37:09 -0700

    SleepNightAdapter.kt

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -8,3 +8,20 @@ import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.database.SleepNight
 
 
+class SleepNightAdapter: RecyclerView.Adapter<TextItemViewHolder>(){
+
+    var data = listOf<SleepNight>
+
+    override fun getItemCount() = data.size
+
+    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+
+        val item = data[position]
+
+        holder.textView.text = item.sleepQuality.toString()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+        TODO("Not yet implemented")
+    }
+}
